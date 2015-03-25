1391ce07643bccc483fc03d3172eb0b922d33bc7
Sanders Lee <slee_colonel@hotmail.com>
2015-03-24 19:25:10 -0700

    Added and integrated TinyMCE for rich text editing.

--- application/views/_template.php
+++ application/views/_template.php
@@ -16,6 +16,13 @@ if (!defined('APPPATH'))
         <title>{title}</title>
         <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <script src="/assets/js/tiny_mce/tiny_mce.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            tinyMCE.init({
+                selector: "textarea",
+                content_css: "/assets/css/bootstrap.min.css"
+            });
+        </script>
         <link href="/assets/css/bootstrap.min.css" rel="stylesheet" media="screen"/>
         <link rel="stylesheet" type="text/css" href="/assets/css/style.css"/>
         {caboose_styles}
